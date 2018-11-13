a45b6d07d39442c3bd8024e575ff59e3686bc8f2
BAYRAMOV Matin <bayramov.matin@gmail.com>
2018-11-13 22:27:37 +0100

    Removed customer and agreement models, as we don't use them for instance.

--- src/main/scala/thbaymet/github/io/models/CustomerAgreement.scala
+++
@@ -1,9 +0,0 @@
-package thbaymet.github.io.models
-
-case
-class
-CustomerAgreement
-(
-  customer: Customer,
-  agreement: Agreement
-)
--- src/main/scala/thbaymet/github/io/models/models.scala
+++ src/main/scala/thbaymet/github/io/models/models.scala
@@ -2,16 +2,6 @@ package thbaymet.github.io.models
 
 case
 class
-Customer
-(
-  id: String,
-  firstName: String,
-  lastName: String,
-  birthday: String
-)
-
-case
-class
 User
 (
   gender: String,
