b2626abe2a41d4adcf994f1155ca9f4af468805f
thbaymet <bayramov.matin@gmail.com>
2019-02-24 22:11:43 +0100

    Added new sql functions.

--- src/main/scala/thbaymet/github/io/udfs/SparkSqlUdfs.scala
+++ src/main/scala/thbaymet/github/io/udfs/SparkSqlUdfs.scala
@@ -7,7 +7,7 @@ object SparkSqlUdfs extends scala.App with LocalSparkSession {
 
   import spark.implicits._
 
-  val data = Seq("John")
+  val data = Seq("John", "Arnold", "Tom", "Xavier")
   val schema = Seq("name")
 
   val df = spark.sparkContext.parallelize(data).toDF(schema :_ *)
@@ -15,10 +15,14 @@ object SparkSqlUdfs extends scala.App with LocalSparkSession {
   df.printSchema()
   df.show()
 
+  def randomNumber = udf(() => (Math.random * 100).toInt - 63)
+  def nullValue = udf(() => if (Math.random * 100 > 85) null else "-")
+
   val dfs = df
     .withColumn("col", col("name"))
     .withColumn("column", column("name"))
     .withColumn("lit", lit("literal"))
+    .withColumn("null", nullValue())
     .withColumn("trim", trim(col("name")))
     .withColumn("ltrim", ltrim(col("name")))
     .withColumn("rtrim", rtrim(col("name")))
@@ -26,9 +30,13 @@ object SparkSqlUdfs extends scala.App with LocalSparkSession {
     .withColumn("upper", upper(col("name")))
     .withColumn("current_date", current_date())
     .withColumn("current_timestamp", current_timestamp())
-    .withColumn("custom_date", lit("2019/02/22"))
     .withColumn("to_date", to_date(col("current_timestamp")))
+    .withColumn("custom_date", lit("2019/02/22"))
     .withColumn("to_date/fmt", to_date(col("custom_date"), "yyyy/MM/dd"))
+    .withColumn("random_number", randomNumber())
+    .withColumn("abs_random_number", abs(randomNumber()))
+    .withColumn("array", array("lit", "current_date"))
+    .withColumn("coalesce", coalesce(col("null"), col("lit")))
 
   dfs.printSchema()
   dfs.show()
